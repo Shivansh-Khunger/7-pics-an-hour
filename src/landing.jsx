c819886805aa584752bf1491339d957a340e07c7
Shivansh Khunger <khunger.shivansh@outlook.com>
2023-11-02 12:49:28 +0530

    added the link to unsplash

--- src/landing.jsx
+++ src/landing.jsx
@@ -4,7 +4,7 @@ function Landing() {
     return (
         <section className="h-[25vh] flex justify-center items-center prose prose-xl min-w-full hover:drop-shadow-lg shadow-cyan-300">
             <h1 className="">7 pics of the hour are ...</h1>
-            <h4 className="text-primary">powered by <a className="text-primary" href="">Unsplash</a></h4>
+            <h4 className="text-primary">powered by <a className="text-primary" href="https://unsplash.com/" target="_blank">Unsplash</a></h4>
         </section>
     )
 }
