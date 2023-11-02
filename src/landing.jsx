e6dcfa7e02a83a3783d494d4d9d3979aa487e636
Shivansh Khunger <khunger.shivansh@outlook.com>
2023-11-02 12:57:05 +0530

    modified a bit more

--- src/landing.jsx
+++ src/landing.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Landing() {
     return (
         <section className="h-[25vh] flex justify-center items-center prose prose-xl min-w-full hover:drop-shadow-lg shadow-cyan-300">
-            <h1 className="">7 pics of the hour are ...</h1>
+            <h1 className="">7 random pics are ...</h1>
             <h4 className="text-primary">powered by <a className="text-primary" href="https://unsplash.com/" target="_blank">Unsplash</a></h4>
         </section>
     )
